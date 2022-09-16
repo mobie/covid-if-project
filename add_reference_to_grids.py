f5b2d12f2cc155412b1965c6da55932b47a0c3a6
Constantin Pape <c.pape@gmx.net>
2022-09-16 16:07:56 +0200

    Add metadata source to the grid views

---
+++ add_reference_to_grids.py
@@ -0,0 +1,35 @@
+import mobie
+
+
+def add_reference_to_grids_in_view(view):
+    if "sourceTransforms" not in view:
+        return view
+    source_transforms = view["sourceTransforms"]
+    new_source_transforms = []
+    references = {}
+    for trafo in source_transforms:
+        trafo_type, trafo = next(iter(trafo.items()))
+        if trafo_type == "mergedGrid":
+            sources = trafo["sources"]
+            source_prefix = sources[0].split("_")[0]
+            if source_prefix not in references:
+                references[source_prefix] = sources[0]
+            trafo["metadataSource"] = references[source_prefix]
+        trafo = {trafo_type: trafo}
+        new_source_transforms.append(trafo)
+    view["sourceTransforms"] = new_source_transforms
+    return view
+
+
+def main():
+    ds_folder = "./data/20200406_164555_328"
+    metadata = mobie.metadata.read_dataset_metadata(ds_folder)
+    views = metadata["views"]
+    for name, view in views.items():
+        updated_view = add_reference_to_grids_in_view(view)
+        views[name] = updated_view
+    mobie.metadata.write_dataset_metadata(ds_folder, metadata)
+
+
+if __name__ == "__main__":
+    main()
